e15dd91b96a3a00b2061231eac052513e4735033
noyex <153921909+noyex@users.noreply.github.com>
2025-03-10 11:50:30 +0100

    project set up

---
+++ FocusFlow-backend/src/main/java/com/noyex/focusflowbackend/FocusFlowBackendApplication.java
@@ -0,0 +1,13 @@
+package com.noyex.focusflowbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FocusFlowBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FocusFlowBackendApplication.class, args);
+    }
+
+}
---
+++ FocusFlow-backend/src/test/java/com/noyex/focusflowbackend/FocusFlowBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noyex.focusflowbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FocusFlowBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
